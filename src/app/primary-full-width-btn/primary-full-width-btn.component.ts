738242d6986e763ceb508e3a257ca736b3f6bf73
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-06-14 08:13:36 -0500

    this is a presentational component so this file is empty

---
+++ src/app/primary-full-width-btn/primary-full-width-btn.component.ts
@@ -0,0 +1,9 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'ng-primary-full-width-btn',
+  templateUrl: './primary-full-width-btn.component.html',
+  styleUrls: ['./primary-full-width-btn.component.scss']
+})
+export class PrimaryFullWidthBtnComponent {
+}
