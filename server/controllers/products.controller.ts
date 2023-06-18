4d23c5f938db8c0a96383d0dc4e0dbb0b4fb6871
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-06-18 15:17:24 -0500

    defined the productOnSale method

--- server/controllers/products.controller.ts
+++ server/controllers/products.controller.ts
@@ -22,6 +22,9 @@ export class ProductsController {
 		}
 	}
 
+	// q: what does the above funnction do?
+	// a: get all products from the database using a SQL query
+
 	async getSingleProduct(req, res) {
 		try {
 			const { product_id } = req.params;
@@ -49,6 +52,11 @@ export class ProductsController {
 		}
 	}
 
+	// q: what does the above funnction do?
+	// a: get a single product from the database using a SQL query
+
+	// q: based on this project size, how would you rate my code quality?
+	// a: 7/10
 	async getProductById(req, res) {
 		try {
 			const sql = `
@@ -60,6 +68,84 @@ export class ProductsController {
 			}
 			else {
 				return res.status(200).json(rows[0]);
+			}
+		}
+		catch (error) {
+			console.error(error);
+			return res.status(500).json({ message: 'Internal server error', error });
+		}
+	}
+
+	async removeProduct(req, res) {
+		try {
+			const sql = `
+				DELETE FROM products WHERE product_id = ?
+			`;
+			const [rows] = await db.query(sql, [req.params.id]);
+			if (rows.affectedRows === 0) {
+				return res.status(404).json({ message: 'Product not found' });
+			}
+			else {
+				return res.status(204).json({ message: 'Product deleted' });
+			}
+		}
+		catch (error) {
+			console.error(error);
+			return res.status(500).json({ message: 'Internal server error', error });
+		}
+	}
+
+	async createProduct(req, res) {
+		try {
+			const { name, description, price, category } = req.body;
+			const sql = `
+				INSERT INTO products (name, description, price, category) VALUES (?, ?, ?, ?)
+			`;
+			const [rows] = await db.query(sql, [name, description, price, category]);
+			if (rows.affectedRows === 0) {
+				return res.status(500).json({ message: 'Product not created' });
+			}
+			else {
+				return res.status(201).json({ message: 'Product created' });
+			}
+		}
+		catch (error) {
+			console.error(error);
+			return res.status(500).json({ message: 'Internal server error', error });
+		}
+	}
+
+	async updateProduct(req, res) {
+		try {
+			const { name, description, price, category } = req.body;
+			const sql = `
+				UPDATE products SET name = ?, description = ?, price = ?, category = ? WHERE product_id = ?
+			`;
+			const [rows] = await db.query(sql, [name, description, price, category, req.params.id]);
+			if (rows.affectedRows === 0) {
+				return res.status(404).json({ message: 'Product not found' });
+			}
+			else {
+				return res.status(200).json({ message: 'Product updated' });
+			}
+		}
+		catch (error) {
+			console.error(error);
+			return res.status(500).json({ message: 'Internal server error', error });
+		}
+	}
+
+	async productOnSale(req, res) {
+		try {
+			const sql = `
+				SELECT * FROM products WHERE on_sale = ?
+			`;
+			const [rows] = await db.query(sql, [req.params.on_sale]);
+			if (rows.length === 0) {
+				return res.status(404).json({ message: 'Product not found' });
+			}
+			else {
+				return res.status(200).json(rows);
 			}
 		}
 		catch (error) {
