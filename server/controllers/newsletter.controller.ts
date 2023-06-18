89e1663a983b40860780e06a26ccbb92148f5b1d
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-06-18 15:05:31 -0500

    refactored the controller and defined just methods subscribe and unsubscribe

--- server/controllers/newsletter.controller.ts
+++ server/controllers/newsletter.controller.ts
@@ -1,31 +1,15 @@
 // Date: 07/27/2021
 // @ts-ignore
 import { db } from '../server';
+const nodemailer = require('nodemailer');
 export class NewsletterController {
-	async getAllNewsletters(req, res) {
+	async subscribe(req, res) {
 		try {
+			const { email } = req.body;
 			const sql = `
-				SELECT * FROM newsletter_subscribers
+				INSERT INTO newsletter_subscribers (email) VALUES (?)
 			`;
-			const [rows] = await db.query(sql);
-			if (rows.length === 0) {
-				return res.status(404).json({ message: 'Newsletters not found' });
-			}
-			else {
-				return res.status(200).json(rows);
-			}
-		}
-		catch (error) {
-			console.error(error);
-			return res.status(500).json({ message: 'Internal server error', error });
-		}
-	}
-	async getNewsletterById(req, res) {
-		try {
-			const sql = `
-				SELECT * FROM newsletter WHERE newsletter_id = ?
-			`;
-			const [rows] = await db.query(sql, [req.params.id]);
+			const [rows] = await db.query(sql, [email]);
 			if (rows.length === 0) {
 				return res.status(404).json({ message: 'Newsletter not found' });
 			}
@@ -38,30 +22,8 @@ export class NewsletterController {
 			return res.status(500).json({ message: 'Internal server error', error });
 		}
 	}
-	async createNewsletter(req, res) {
-		try {
-			const { title, content, created_at, updated_at } = req.body;
-			const sql = `
-				INSERT INTO newsletter_subscribers (subscriber_id, email, created_at, updated_at)
-				VALUES (?, ?, ?, ?)
-			`;
-			const [{ insertId }] = await db.query(sql, [title, content, created_at, updated_at]);
-			const sql2 = `
-				SELECT * FROM newsletter WHERE newsletter_id = ?
-			`;
-			const [rows] = await db.query(sql2, [insertId]);
-			if (rows.length === 0) {
-				return res.status(404).json({ message: 'Newsletter not found' });
-			}
-			else {
-				return res.status(200).json(rows[0]);
-			}
-		}
-		catch (error) {
-			console.error(error);
-			return res.status(500).json({ message: 'Internal server error', error });
-		}
-	}
+
+
 
 	async unsubscribe(req, res) {
 		try {
@@ -83,7 +45,26 @@ export class NewsletterController {
 		}
 	}
 
-	// q: what does the above function do?
-	// a: it deletes the email from the newsletter_subscribers table, effectively
-	//    unsubscribing the user from the newsletter
+	// q: can you explain to me what sendNewsletter does?
+	// a: it sends a newsletter to all subscribers.
+	async getAllSubscribers(req, res) {
+		try {
+			const sql = `
+				SELECT * FROM newsletter_subscribers
+			`;
+			const [rows] = await db.query(sql);
+			if (rows.length === 0) {
+				return res.status(404).json({ message: 'Newsletters not found' });
+			}
+			else {
+				return res.status(200).json(rows);
+			}
+		}
+		catch (error) {
+			console.error(error);
+			return res.status(500).json({ message: 'Internal server error', error });
+		}
+	}
+
 }
+
